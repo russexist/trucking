d29182211852d10f0f97aba806dd672532860170
russexist <jambo29@yandex.com>
2018-02-02 23:55:46 +0200

    fix navbar / devise update user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,14 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone, :driver])
+      devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:email, :password,
+      :password_confirmation, :remember_me, :first_name, :last_name, :phone, :driver)
+      end
+
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:email, :password,
+      :password_confirmation, :current_password, :first_name, :last_name, :phone, :driver)
+      end
     end
 end
