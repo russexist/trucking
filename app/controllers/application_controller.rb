e0cf1852b54b4791711691a983915feeb9ecc306
russexist <jambo29@yandex.com>
2018-02-05 20:28:13 +0200

    add user profile / add favicon

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,27 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def profile
+    if params[:driver]
+      @user = User.find_by(id: params[:driver])
+    else
+      @user = current_user
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:email, :password,
-               :password_confirmation, :remember_me, :first_name,
-               :last_name, :phone, :driver)
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password,
+                         :password_confirmation, :remember_me, :first_name,
+                         :last_name, :phone, :driver)
     end
 
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:email, :password,
-               :password_confirmation, :current_password,
-               :first_name, :last_name, :phone, :driver)
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit(:email, :password,
+                         :password_confirmation, :current_password,
+                         :first_name, :last_name, :phone, :driver)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/change_status', to: 'orders#change_status'
   get '/taken_orders', to: 'orders#taken_orders'
+  get '/profile', to: 'application#profile'
 
   root 'orders#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
