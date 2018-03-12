8c6a5e0e241a4b183e1b67674476f8391de92f0d
russexist <jambo29@yandex.com>
2018-03-12 16:55:56 +0200

    change name js variables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,41 +29,33 @@ ready = function() {
   });
   datepickerRu();
 
-  // MultiStepForm
-  var current_fs, next_fs, previous_fs; //fieldsets
-  var left, opacity, scale; //fieldset properties which we will animate
-  var animating; //flag to prevent quick multi-click glitches
+  var currentFs, nextFs, previousFs;
+  var left, opacity, scale; 
+  var animating;
 
   $(".next").click(function(){
     if(animating) return false;
     animating = true;
 
-    current_fs = $(this).parent();
-    next_fs = $(this).parent().next();
+    currentFs = $(this).parent();
+    nextFs = $(this).parent().next();
 
-    //show the next fieldset
-    next_fs.show();
+    nextFs.show();
     $('.active').next().addClass('active');
 
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
+    currentFs.animate({opacity: 0}, {
       step: function(now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale current_fs down to 80%
         scale = 1 - (1 - now) * 0.2;
-        //2. bring next_fs from the right(50%)
         left = (now * 50)+"%";
-        //3. increase opacity of next_fs to 1 as it moves in
         opacity = 1 - now;
-        current_fs.css({'transform': 'scale('+scale+')'});
-        next_fs.css({'left': left, 'opacity': opacity});
+        currentFs.css({'transform': 'scale('+scale+')'});
+        nextFs.css({'left': left, 'opacity': opacity});
       },
       duration: 800,
       complete: function(){
-        current_fs.hide();
+        currentFs.hide();
         animating = false;
       },
-      //this comes from the custom easing plugin
       easing: 'easeInOutBack'
     });
   });
@@ -72,11 +64,10 @@ ready = function() {
     if(animating) return false;
     animating = true;
 
-    current_fs = $(this).parent();
-    previous_fs = $(this).parent().prev();
+    currentFs = $(this).parent();
+    previousFs = $(this).parent().prev();
 
-    //show the previous fieldset
-    previous_fs.show();
+    previousFs.show();
 
     if ($('.last').hasClass('active')){
       $('.last').removeClass('active');
@@ -85,25 +76,19 @@ ready = function() {
       $('.second').removeClass('active');
     }
 
-    //hide the current fieldset with style
-    current_fs.animate({opacity: 0}, {
+    currentFs.animate({opacity: 0}, {
       step: function(now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale previous_fs from 80% to 100%
         scale = 0.8 + (1 - now) * 0.2;
-        //2. take current_fs to the right(50%) - from 0%
         left = ((1-now) * 50)+"%";
-        //3. increase opacity of previous_fs to 1 as it moves in
         opacity = 1 - now;
-        current_fs.css({'left': left});
-        previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+        currentFs.css({'left': left});
+        previousFs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
       },
       duration: 800,
       complete: function(){
-        current_fs.hide();
+        currentFs.hide();
         animating = false;
       },
-      //this comes from the custom easing plugin
       easing: 'easeInOutBack'
     });
   });
